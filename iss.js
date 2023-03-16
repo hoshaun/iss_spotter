eef7fa9f653de556db908d521e05f66f1fe27f2a
hoshaun <shaun.ho@mail.utoronto.ca>
2023-03-16 14:30:35 -0400

    submit fetchMyIP

---
+++ iss.js
@@ -0,0 +1,30 @@
+const request = require('request');
+
+/**
+ * Makes a single API request to retrieve the user's IP address.
+ * Input:
+ *   - A callback (to pass back an error or the IP string)
+ * Returns (via Callback):
+ *   - An error, if any (nullable)
+ *   - The IP address as a string (null if error). Example: "162.245.144.188"
+ */
+const fetchMyIP = function(callback) {
+  request.get('https://api.ipify.org?format=json', (error, response, body) => {
+    if (error) {
+      callback(error, null);
+      return;
+    }
+
+    if (response.statusCode !== 200) {
+      const msg = `Status Code ${response.statusCode} when fetching IP. Response: ${body}`;
+      callback(Error(msg), null);
+      return;
+    }
+
+    const ip = body && JSON.parse(body) ? JSON.parse(body).ip : null;
+
+    callback(error, ip);
+  });
+};
+
+module.exports = { fetchMyIP };
